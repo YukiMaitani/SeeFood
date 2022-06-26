03b79b253f20850de6bebc6f524c8311c03b23bc
yuki_maitani <fightzoi1997@gmail.com>
2022-06-26 22:42:47 +0900

    カメラで撮影し表示

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -9,14 +9,27 @@ import UIKit
 import CoreML
 import Vision
 
-class ViewController: UIViewController,UIPickerViewDelegate,UINavigationControllerDelegate {
+class ViewController: UIViewController,UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    let imagePicker = UIImagePickerController()
 
     @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedImage = info[.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+        }
+        imagePicker.dismiss(animated: true)
     }
 
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true)
     }
     
 }
