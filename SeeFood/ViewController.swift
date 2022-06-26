2354821574e747ac8d4d4f7bfb380dbb00a0166a
yuki_maitani <fightzoi1997@gmail.com>
2022-06-27 01:55:23 +0900

    画像分類処理の実装

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -24,8 +24,33 @@ class ViewController: UIViewController,UINavigationControllerDelegate, UIImagePi
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let userPickedImage = info[.originalImage] as? UIImage {
             imageView.image = userPickedImage
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("ciimageの作成に失敗しました")
+            }
+            detect(ciimage: ciimage)
         }
         imagePicker.dismiss(animated: true)
+    }
+    
+    func detect(ciimage:CIImage) {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3_2().model) else {
+            fatalError("モデルの読み込みに失敗しました")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { request, error in
+            guard let results = request.results else {
+                fatalError("リクエストが失敗しました")
+            }
+            print(results)
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: ciimage)
+        do {
+            try handler.perform([request])
+        }
+        catch {
+            print("画像解析に失敗しました\(error)")
+        }
     }
 
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
